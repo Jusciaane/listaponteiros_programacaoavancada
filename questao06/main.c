15e8392f1b0bd686163129381ac27ef3544ed068
Jusciaane <43283477+Jusciaane@users.noreply.github.com>
2022-09-23 15:07:34 -0300

    Create main.c

---
+++ questao06/main.c
@@ -0,0 +1,11 @@
+// Questão 06
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+//Dado um vetor pulo qualquer: int pulo [10]={1,2,3,4,5,6,7,8,9,10};
+    *(pulo + 2);// Endereço de memória da terceira posição
+    *(pulo + 4);// Endereço de memória da quinta posição
+    pulo + 4; // Faz referência ao valor da quinta posição
+    pulo + 2;// Faz referência ao valor da terceira posição
+return (0);
+}
