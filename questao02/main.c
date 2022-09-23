4116bb13143ed9ede26866906bd9272f9d64b26c
Jusciaane <43283477+Jusciaane@users.noreply.github.com>
2022-09-22 22:02:48 -0300

    Update main.c

--- questao02/main.c
+++ questao02/main.c
@@ -1,1 +1,10 @@
 // Questão 02
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+//Dado no enunciado
+int i=5, *p;
+p = &i;
+printf("%x %d %d %d %d", p,*p+2,**&p,3**p,**&p+4); //61fe1c 7 5 15 9
+return 0;
+}
