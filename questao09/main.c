dda7b7c78f04c94a43571e6a43b3dc9702e6162a
Jusciaane <43283477+Jusciaane@users.noreply.github.com>
2022-09-23 16:32:46 -0300

    Update main.c

--- questao09/main.c
+++ questao09/main.c
@@ -1,1 +1,27 @@
 //Questão 09
+
+//Posição inicial do x[0]: 4092
+
+//Caso 1: char x[4]
+//1 byte na memória (anda a cada 1 byte)
+//Resposta: x+1: 4093
+//Resposta: x+2: 4094
+//Resposta: x+3: 4095
+
+//Caso 2: int x[4]
+//2 bytes na memória (anda a cada 2 bytes)
+//Resposta: x+1: 4094
+//Resposta: x+2: 4096
+//Resposta: x+3: 4098
+
+//Caso 3: float x[4]
+//4 bytes na memória (anda a cada 4 bytes)
+//Resposta: x+1: 4096
+//Resposta: x+2: 4100
+//Resposta: x+3: 4104
+
+//Caso 4: double x[4]
+//8 bytes na memória (anda a cada 8 bytes)
+//Resposta: x+1: 4100
+//Resposta: x+2: 4108
+//Resposta: x+3: 4116
