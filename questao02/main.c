4e3b0c64f9af893ad0638eab0361f56095a9c95a
Jusciaane <43283477+Jusciaane@users.noreply.github.com>
2022-09-22 22:16:43 -0300

    Update main.c

--- questao02/main.c
+++ questao02/main.c
@@ -5,6 +5,12 @@ int main(){
 //Dado no enunciado
 int i=5, *p;
 p = &i;
-printf("%x %d %d %d %d", p,*p+2,**&p,3**p,**&p+4); //61fe1c 7 5 15 9
+printf("%x %d %d %d %d", p,*p+2,**&p,3**p,**&p+4);
+//saídas: 61fe1c 7 5 15 9
+//61fe1c: endereço em base hexadecimal
+//7: valor de i(5)+2
+//5: recupera o conteúdo do endereço de p
+//15: conteúdo de p multiplicado por 3
+//9: recupera o conteúdo do endereço de p e soma mais 4
 return 0;
 }
