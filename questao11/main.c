fa9c58851b7a19954b556a6bdd851a5effb5740e
Jusciaane <43283477+Jusciaane@users.noreply.github.com>
2022-09-23 20:26:57 -0300

    Update main.c

--- questao11/main.c
+++ questao11/main.c
@@ -1,1 +1,17 @@
-//Questão 11
+// Questão 11
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+float aloha[10], coisas[10][5], *pf, value = 2.2;
+int i=3;
+//////////////
+aloha[2] = value;// Válido.
+scanf("%f", &aloha); // Válido.
+aloha = value"; // Inválido, pois o ponteiro aloha não pode receber o value (inteiro)
+printf("%f", aloha);// Válido.
+coisas[4][4] = aloha[3];// Válido.
+coisas[5] = aloha;// Inválido, pois coisa só pode receber valor e não endereço..
+pf = value;// Inválido, pois como pf é ponteiro a atribuição não pode ser para receber um valor.
+pf = aloha; // Válido.
+return (0);
+}
