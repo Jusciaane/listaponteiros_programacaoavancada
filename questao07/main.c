bb28f36d98763fc78181956733858db5d9a55afc
Jusciaane <43283477+Jusciaane@users.noreply.github.com>
2022-09-23 21:00:55 -0300

    Update main.c

--- questao07/main.c
+++ questao07/main.c
@@ -1,1 +1,14 @@
-//Questão 07
+// Questão 07 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+int mat[4], *p, x;
+p = mat + 1; //Válido
+p = mat++;//Inválido, pois não é possível, por meio dessa sentença, deslocar o endereço de mat.
+p = ++mat;//Inválido, pois não é possível, por meio dessa sentença, deslocar o endereço de mat.
+x = (*mat)++;//Válido
+
+return (0);
+}
